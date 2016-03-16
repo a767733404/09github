9fd64b9bdd067fe531c546f01d23977961d64b1e
wyl <lan@ZZH.local>
2016-03-16 10:15:45 +0800

    搭建框架

---
+++ github/github/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  github
+//
+//  Created by lanou on 16/3/16.
+//  Copyright © 2016年 张志华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ github/github/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  github
+//
+//  Created by lanou on 16/3/16.
+//  Copyright © 2016年 张志华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
